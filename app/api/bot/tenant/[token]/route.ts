e393a3f45183bfe18f633da123fb548872fb3a90
tunogya <tunogya@qq.com>
2025-01-03 03:17:07 +0800

    update

--- app/api/bot/tenant/[token]/route.ts
+++ app/api/bot/tenant/[token]/route.ts
@@ -1,9 +1,11 @@
 import {NextRequest} from "next/server";
 
-const POST = async (req: NextRequest) => {
+const POST = async (req: NextRequest, {params}: { params: { token: string } }) => {
+  const {token} = await params;
   const body = await req.json()
 
   console.log(body)
+  console.log(token)
 
   return Response.json({
     ok: true,
