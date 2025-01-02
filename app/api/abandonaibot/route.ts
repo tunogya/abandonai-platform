1b7f3b6678fc45a99ca9b11441df7e56824f55d6
tunogya <tunogya@qq.com>
2025-01-03 02:05:43 +0800

    update

---
+++ app/api/abandonaibot/route.ts
@@ -0,0 +1,17 @@
+export const dynamic = 'force-dynamic'
+
+export const fetchCache = 'force-no-store'
+
+import {Bot, webhookCallback} from 'grammy'
+
+const token = process.env.TELEGRAM_BOT_TOKEN
+
+if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
+
+const bot = new Bot(token)
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello, world!");
+});
+
+export const POST = webhookCallback(bot, 'std/http')
