192248f4391b57d82a474da59241367dbd6d1e24
tunogya <tunogya@qq.com>
2025-01-03 03:28:14 +0800

    update

--- app/api/bot/platform/route.ts
+++ app/api/bot/platform/route.ts
@@ -14,4 +14,30 @@ bot.command("start", async (ctx) => {
   await ctx.reply(JSON.stringify(ctx));
 });
 
+/**
+ * command - newagent
+ *
+ * command - myagents
+ */
+
+/**
+ * Edit Agent
+ *
+ * ...
+ *
+ * command - deleteagent
+ */
+
+/**
+ * Agent Settings
+ *
+ * command -
+ */
+
+/**
+ * Knowledge bases
+ */
+
+
+
 export const POST = webhookCallback(bot, 'std/http')
