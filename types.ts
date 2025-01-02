5da6be068832f8494f68ec54aa42f791ed870700
tunogya <tunogya@qq.com>
2025-01-03 00:35:09 +0800

    add telegram webapps

---
+++ types.ts
@@ -0,0 +1,85 @@
+export interface IWebAppUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  language_code: string;
+}
+
+// https://core.telegram.org/bots/webapps#initializing-mini-apps
+export interface IWebApp {
+  initData: string;
+  initDataUnsafe: {
+    query_id: string;
+    user: IWebAppUser;
+    auth_date: string;
+    hash: string;
+  };
+  version: string;
+  platform: string;
+  colorScheme: string;
+  themeParams: {
+    link_color: string;
+    button_color: string;
+    button_text_color: string;
+    secondary_bg_color: string;
+    hint_color: string;
+    bg_color: string;
+    text_color: string;
+  };
+  isActive: boolean;
+  isExpanded: boolean;
+  viewportHeight: number;
+  viewportStableHeight: number;
+  isClosingConfirmationEnabled: boolean;
+  headerColor: string;
+  backgroundColor: string;
+  bottomBarColor: string;
+  isVerticalSwipesEnabled: boolean;
+  isFullscreen: boolean;
+  isOrientationLocked: boolean;
+  safeAreaInset: {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
+  contentSafeAreaInset: {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
+  BackButton: {
+    isVisible: boolean;
+  };
+  MainButton: {
+    text: string;
+    color: string;
+    textColor: string;
+    isVisible: boolean;
+    isProgressVisible: boolean;
+    isActive: boolean;
+  };
+  HapticFeedback: any;
+  Accelerometer: any;
+  DeviceOrientation: any;
+  Gyroscope: any;
+  LocationManager: any;
+  isVersionAtLeast: (version: string) => void;
+  setHeaderColor: (color: string) => void;
+  setBackgroundColor: (color: string) => void;
+  setBottomBarColor: (color: string) => void;
+  enableClosingConfirmation: () => void;
+  disableClosingConfirmation: () => void;
+  enableVerticalSwipes: () => void;
+  disableVerticalSwipes: () => void;
+  requestFullscreen: () => void;
+  exitFullscreen: () => void;
+  lockOrientation: () => void;
+  unlockOrientation: () => void;
+  addToHomeScreen: () => void;
+  sendData: (data: string) => void;
+  expand: () => void;
+  close: () => void;
+}
