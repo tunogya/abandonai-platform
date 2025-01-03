04e6abc246e8129a804237b7fe6a43597e304488
tunogya <tunogya@qq.com>
2025-01-04 00:14:42 +0800

    update

--- app/api/bot/platform/route.ts
+++ app/api/bot/platform/route.ts
@@ -38,7 +38,7 @@ bot.use(async (ctx, next) => {
  */
 bot.command("start", async (ctx) => {
   // const payload = ctx.match
-  await ctx.reply(`I can help you create and manage Agents.
+  await ctx.reply(`I can help you create and manage Agents\.
 
 You can control me by sending these commands:
 
