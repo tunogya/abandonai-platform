7fb1e3c17d2451b0d294eb086f8a8816024c9a17
tunogya <tunogya@qq.com>
2025-01-03 23:47:33 +0800

    update

--- app/api/bot/platform/route.ts
+++ app/api/bot/platform/route.ts
@@ -2,23 +2,83 @@ export const dynamic = 'force-dynamic'
 
 export const fetchCache = 'force-no-store'
 
-import {Bot, webhookCallback} from 'grammy'
+import {Bot, Context, webhookCallback} from 'grammy'
+
+const BOT_DEVELOPER = 2130493951;
+
+// Define custom context type.
+interface BotConfig {
+  botDeveloper: number;
+  isDeveloper: boolean;
+}
+
+type MyContext = Context & {
+  config: BotConfig;
+};
 
 const token = process.env.TELEGRAM_BOT_TOKEN
 
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
-const bot = new Bot(token)
+const bot = new Bot<MyContext>(token)
 
+/**
+ * Middleware to add bot config to context.
+ */
+bot.use(async (ctx, next) => {
+  ctx.config = {
+    botDeveloper: BOT_DEVELOPER,
+    isDeveloper: ctx.from?.id === BOT_DEVELOPER,
+  };
+  await next();
+});
+
+/**
+ * command - start
+ */
 bot.command("start", async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx));
+  // const payload = ctx.match
+  await ctx.reply(`I can help you create and manage Agents.
+
+You can control me by sending these commands:
+
+/newagent - create a new agent
+/myagents - edit your agents
+
+*Edit Agents*
+/deleteagent - delete a agent
+
+*Agent Settings*
+
+*Knowledge bases*
+`, {
+    parse_mode: "MarkdownV2"
+  });
 });
 
 /**
  * command - newagent
- *
+ */
+
+bot.command("newagent", async (ctx) => {
+  if (!ctx.config.isDeveloper) {
+    await ctx.reply("You are not authorized to create a new agent.");
+    return;
+  }
+  await ctx.reply("Creating new agent...");
+})
+
+/**
  * command - myagents
  */
+
+bot.command("myagents", async (ctx) => {
+  if (!ctx.config.isDeveloper) {
+    await ctx.reply("You are not authorized to create a new agent.");
+    return;
+  }
+  await ctx.reply("Fetching your agents...");
+})
 
 /**
  * Edit Agent
