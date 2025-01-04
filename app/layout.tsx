3388b2dde4e8903af0a4c38dcc104110721462dd
tunogya <tunogya@qq.com>
2025-01-05 05:18:26 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased select-none h-screen overflow-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden`}
       >
         {children}
       </body>
--- app/tenant/menu/page.tsx
+++ app/tenant/menu/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
                   d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z"/>
               </svg>
             </div>
-            <div className={"text-[12px]"}>Video Call</div>
+            <div className={"text-[12px] select-none"}>Video Call</div>
           </div>
         </div>
         <div style={{
