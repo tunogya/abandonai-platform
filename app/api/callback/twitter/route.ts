9ad9921c2f05ac32bd0832c5147f982ff7b10a96
tunogya <tunogya@qq.com>
2025-01-12 16:52:58 +0800

    update

--- app/api/callback/twitter/route.ts
+++ app/api/callback/twitter/route.ts
@@ -3,13 +3,19 @@ import {TwitterApi} from "twitter-api-v2";
 import {NextRequest} from "next/server";
 
 const redis = Redis.fromEnv();
-const twitterClient = new TwitterApi({ clientId: process.env.X_CLIENT_ID || "", clientSecret: process.env.X_CLIENT_SECRET || "" });
+const twitterClient = new TwitterApi({
+  clientId: process.env.X_CLIENT_ID || "",
+  clientSecret: process.env.X_CLIENT_SECRET || ""
+});
 
 const GET = async (req: NextRequest) => {
   const searchParams = req.nextUrl.searchParams
   const code = searchParams.get('code');
   const state = searchParams.get('state');
-  const { codeVerifier, state: stateVerifier } = await redis.get(`oauth2:${state}`) as { codeVerifier: string, state: string };
+  const {codeVerifier, state: stateVerifier} = await redis.get(`oauth2:${state}`) as {
+    codeVerifier: string,
+    state: string
+  };
   if (!code || !state || state !== stateVerifier) {
     return Response.json({message: 'error'});
   }
@@ -17,7 +23,7 @@ const GET = async (req: NextRequest) => {
     code: `${code}`, codeVerifier, redirectUri: "https://open.abandon.ai/api/callback/twitter",
   })
   const {client: loggedClient, accessToken, refreshToken, expiresIn} = response;
-  const { data: userObject } = await loggedClient.v2.me();
+  const {data: userObject} = await loggedClient.v2.me();
   await redis.set(`twitterbotauth2:${state}`, {accessToken, refreshToken, expiresIn, userObject});
 
   return Response.redirect("https://t.me/abandonaibot", 302);
