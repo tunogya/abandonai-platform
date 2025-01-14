ed0d152053ec2dc2c846f2a20f49e98064487076
tunogya <tunogya@qq.com>
2025-01-14 20:42:53 +0800

    update

--- app/api/bot/tenant/[id]/route.ts
+++ app/api/bot/tenant/[id]/route.ts
@@ -1,11 +1,23 @@
 import {NextRequest} from "next/server";
+import {
+  BedrockAgentRuntimeClient,
+  InvokeAgentCommand,
+} from "@aws-sdk/client-bedrock-agent-runtime";
+
+const client = new BedrockAgentRuntimeClient({ region: "us-east-1" });
 
 const POST = async (req: NextRequest, {params}: never) => {
   const {id} = await params;
-  const body = await req.json()
+  const body = await req.json();
 
-  console.log(body)
-  console.log(id)
+  const agentId = id;
+  const command = new InvokeAgentCommand({
+    agentId,
+    agentAliasId: undefined,
+    sessionId: undefined,
+    inputText: `${JSON.stringify(body)}`,
+  });
+  await client.send(command);
 
   return Response.json({
     ok: true,
