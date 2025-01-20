88aeedb8e30d4d46d7cdd55b1c972ac717df1e85
tunogya <tunogya@qq.com>
2025-01-21 02:30:01 +0800

    update

--- app/libs/telegram.ts
+++ app/libs/telegram.ts
@@ -1,9 +1,14 @@
 export const getFile = async (file_id: string, bot_token: string) => {
-  const file = await fetch(`https://api.telegram.org/bot${bot_token}/getFile?file_id=${file_id}`).then(res => res.json());
-  const file_path = file.result.file_path;
-  const file_url = `https://api.telegram.org/file/bot${bot_token}/${file_path}`;
-  // TODO: can save file to s3
-  // ...
+  try {
+    const file = await fetch(`https://api.telegram.org/bot${bot_token}/getFile?file_id=${file_id}`).then(res => res.json());
+    const file_path = file.result.file_path;
+    const file_url = `https://api.telegram.org/file/bot${bot_token}/${file_path}`;
+    // TODO: can save file to s3
+    // ...
 
-  return await fetch(file_url).then(res => res.arrayBuffer())
+    return await fetch(file_url).then(res => res.arrayBuffer())
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
