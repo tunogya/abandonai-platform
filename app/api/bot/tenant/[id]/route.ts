da593d10a1f0bc9c8b1441ff09c6f024a00a2774
tunogya <tunogya@qq.com>
2025-01-21 01:50:15 +0800

    update

--- app/api/bot/tenant/[id]/route.ts
+++ app/api/bot/tenant/[id]/route.ts
@@ -1,9 +1,11 @@
 import {NextRequest} from "next/server";
-import {
-  InvokeAgentCommand,
-} from "@aws-sdk/client-bedrock-agent-runtime";
+import {InvokeAgentCommand,} from "@aws-sdk/client-bedrock-agent-runtime";
 import {bedrockAgentRuntimeClient} from "@/app/libs/bedrockAgentRuntimeClient";
 import {redisClient} from "@/app/libs/redisClient";
+import {getFile} from "@/app/libs/telegram";
+import {StartStreamTranscriptionCommand} from "@aws-sdk/client-transcribe-streaming";
+import {Readable} from "node:stream";
+import {transcribeStreamingClient} from "@/app/libs/transcribeStreamingClient";
 
 const BOT_DEVELOPER = 2130493951;
 
@@ -19,24 +21,93 @@ const POST = async (req: NextRequest, {params}: never) => {
   try {
     // 先检查是否有绑定的 agentAliasId
     const agentId = id;
-    const agentAliasId = await redisClient.get(`agentAliasId:${agentId}`) as string | undefined;
-    if (!agentAliasId) {
+
+    const [agentAliasId, botToken] = await Promise.all([
+      redisClient.get(`agentAliasId:${agentId}`),
+      redisClient.get(`telegrambottoken:${agentId}`)
+    ]) as [string | undefined, string | undefined];
+
+    if (!agentAliasId || !botToken) {
       return Response.json({
         ok: false,
-        msg: "agentAliasId not found."
+        msg: "agentAliasId or botToken not found."
       }, {
         status: 404,
       });
     }
+
     body = {
       ...body,
       agent: {
-        agent_id: id,
+        agent_id: agentId,
       }
     }
+
+
     // 判断是否是 voice message
     // 如果是 voice message，则需要先转成文字，再拼接到原来的 body 数据结构中
-    console.log(body);
+    if (body.message?.voice) {
+      const file_id = body.voice.file_id;
+      const file_content = await getFile(file_id, botToken);
+      const readableStream = new Readable({
+        read() {
+          this.push(new Uint8Array(file_content));
+          this.push(null); // 表示数据流结束
+        },
+      });
+      const command = new StartStreamTranscriptionCommand({
+        MediaEncoding: "ogg-opus",
+        MediaSampleRateHertz: 16000,
+        IdentifyMultipleLanguages: true,
+        LanguageOptions: "en-US,zh-CN,id-ID",
+        AudioStream: readableStream,
+      });
+      const data = await transcribeStreamingClient.send(command);
+      if  (!data.TranscriptResultStream) {
+        return Response.json({
+          ok: false,
+          msg: "TranscriptResultStream not found."
+        });
+      }
+      let content = ""
+      for await (const event of data.TranscriptResultStream) {
+        for (const result of event.TranscriptEvent?.Transcript?.Results || []) {
+          if (result.IsPartial === false) {
+            const noOfResults = result?.Alternatives?.[0]?.Items?.length || 0;
+            for (let i = 0; i < noOfResults; i++) {
+              content += result?.Alternatives?.[0]?.Items?.[i].Content || "";
+            }
+          }
+        }
+      }
+      body = {
+        ...body,
+        message: {
+          ...body.message,
+          text: content,
+        },
+      }
+    }
+    if (body.message?.photo) {
+      // https://aws.amazon.com/cn/blogs/china/amazon-bedrock-claude-3-multimodal-usage-guide/
+      const photo = body.photo.some((photo: {width: number, height: number}) => photo.width <= 1568 && photo.height <= 1568 && photo.width > 200 && photo.height > 200);
+      if (!photo) {
+        return Response.json({
+          ok: false,
+          msg: "photo not found."
+        });
+      }
+      const file_id = photo.file_id;
+      const buffer =  await getFile(file_id, botToken)
+      const file_content_base64 = Buffer.from(buffer).toString('base64');
+      body = {
+        ...body,
+        message: {
+          ...body.message,
+          photo: file_content_base64,
+        }
+      }
+    }
 
     const command = new InvokeAgentCommand({
       agentId,
---
+++ app/libs/telegram.ts
@@ -0,0 +1,9 @@
+export const getFile = async (file_id: string, bot_token: string) => {
+  const file = await fetch(`https://api.telegram.org/bot${bot_token}/getFile?file_id=${file_id}`).then(res => res.json());
+  const file_path = file.result.file_path;
+  const file_url = `https://api.telegram.org/file/bot${bot_token}/${file_path}`;
+  // TODO: can save file to s3
+  // ...
+
+  return await fetch(file_url).then(res => res.arrayBuffer())
+}
