1ff49d25a64047ac75ceac87e6400f2bcdc4736b
tunogya <tunogya@qq.com>
2025-01-21 18:38:13 +0800

    update

--- app/tenant/transcribe/page.tsx
+++ app/tenant/transcribe/page.tsx
@@ -26,40 +26,48 @@ const Page = () => {
   // }
 
   return (
-    <div className={"h-screen overflow-hidden flex flex-col"}>
+    <div className={"h-screen overflow-hidden flex flex-col bg-[#D6412D]"}>
       <div style={{
         height: (webApp?.safeAreaInset.top || 0) + (webApp?.contentSafeAreaInset.top || 0)
       }}
       />
-      <div className={"flex justify-end p-3"}>
-        <button className={"p-2"}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-            <path
-              d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z"/>
+      <div className={"flex justify-between p-8 min-h-[100px]"}>
+        <button className={"text-white bg-black bg-opacity-35 w-8 h-8 rounded-full flex items-center justify-center"}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+               stroke="currentColor" className="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
+          </svg>
+        </button>
+        <button className={"text-white w-8 h-8 rounded-full flex items-center justify-center"}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+               stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"/>
           </svg>
         </button>
       </div>
-      <div className={"flex items-center justify-center flex-1"}>
-        <div className={"h-[180px] w-[180px] rounded-full bg-blue-200"}></div>
+      <div
+        className={"flex items-center justify-center flex-1 px-8 py-2 overflow-scroll"}
+        style={{
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
+      >
+        <div className={"w-full h-full flex flex-col"}>
+          <div className={"flex-1 text-white font-bold text-[24px]"}>
+
+            <div className={"h-[200px]"} />
+          </div>
+        </div>
       </div>
       <div className={"w-full px-10 py-4"}>
-        <div className={"flex justify-between"}>
+        <div className={"flex justify-center"}>
           <button
-            className={"h-[66px] w-[66px] rounded-full flex items-center justify-center dark:bg-[#202020] bg-[#FFFFFF] dark:text-[#BCBCBC]"}>
+            className={"h-[66px] w-[66px] rounded-full flex items-center justify-center bg-[#FFFFFF] text-[#D6412D]"}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-              <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z"/>
-              <path
-                d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z"/>
-            </svg>
-          </button>
-          <button
-            onClick={() => {
-              webApp?.close()
-            }}
-            className={"h-[66px] w-[66px] rounded-full flex items-center justify-center dark:bg-[#202020] bg-[#FFFFFF] dark:text-[#BCBCBC]"}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                 stroke="currentColor" className="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+              <path fill-rule="evenodd"
+                    d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                    clip-rule="evenodd"/>
             </svg>
           </button>
         </div>
