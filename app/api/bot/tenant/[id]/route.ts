8558e33aa70142a5c65e6fc477ac33a80ced9a94
tunogya <tunogya@qq.com>
2025-01-30 19:49:12 +0800

    update

--- app/api/bot/tenant/[id]/route.ts
+++ app/api/bot/tenant/[id]/route.ts
@@ -97,7 +97,7 @@ const POST = async (req: NextRequest, {params}: never) => {
 
     if (body.message?.photo) {
       // https://aws.amazon.com/cn/blogs/china/amazon-bedrock-claude-3-multimodal-usage-guide/
-      const photo = body.message.photo.find((photo: {width: number, height: number}) => photo.width <= 1568 && photo.height <= 1568 && photo.width > 200 && photo.height > 200);
+      const photo = body.message.photo.find((photo: {width: number, height: number}) => photo.width <= 1568 && photo.height <= 1568 && photo.width > 240 && photo.height > 240);
       if (!photo) {
         return Response.json({
           ok: true,
