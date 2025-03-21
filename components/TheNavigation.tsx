50f93a78e38fc1278cd3ea4f38070b364056716a
tunogya <tunogya@qq.com>
2025-03-22 05:26:41 +0800

    update

--- components/TheNavigation.tsx
+++ components/TheNavigation.tsx
@@ -92,22 +92,22 @@ const TheNavigation = () => {
               className="mx-5 w-56 origin-top-right rounded-xl border border-gray-100 bg-white py-1 text-xs font-medium text-black transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
             >
               <MenuItem>
-                <Link className="block hover:bg-gray-100 px-3 py-2" href="/profile">
+                <Link className="block hover:bg-gray-100 px-3 py-2" href="/app/profile">
                   Profile
                 </Link>
               </MenuItem>
               <MenuItem>
-                <Link className="block hover:bg-gray-100 px-3 py-2" href="/api-keys">
+                <Link className="block hover:bg-gray-100 px-3 py-2" href="/app/api-keys">
                   API Keys
                 </Link>
               </MenuItem>
               <MenuItem>
-                <Link className="block hover:bg-gray-100 px-3 py-2" href="/webhooks">
+                <Link className="block hover:bg-gray-100 px-3 py-2" href="/app/webhooks">
                   Webhooks
                 </Link>
               </MenuItem>
               <MenuItem>
-                <Link className="block hover:bg-gray-100 px-3 py-2" href="/subscription">
+                <Link className="block hover:bg-gray-100 px-3 py-2" href="/app/subscription">
                   Subscription
                 </Link>
               </MenuItem>
