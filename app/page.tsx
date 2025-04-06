567ba0b8377914fa9ee6297b14655de4ac42c9fa
tunogya <tunogya@qq.com>
2025-04-06 21:38:08 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,12 @@ const Page = async () => {
 
   return (
     <div className={"min-w-screen min-h-screen flex flex-col"}>
-      <div className={"flex-1 flex items-center justify-center py-20 gap-20"}>
-        <div className={""}>
-          <Image
-            width={360}
-            height={360}
-            alt={"banner"}
-            src={"/50FB3804-536E-4012-BE9A-FA869F925AF8.png"}/>
-        </div>
+      <div className={"flex-1 flex items-center justify-center py-20 gap-20 flex-col md:flex-row"}>
+        <Image
+          width={360}
+          height={360}
+          alt={"banner"}
+          src={"/50FB3804-536E-4012-BE9A-FA869F925AF8.png"}/>
         <div className={"w-[360px] flex flex-col gap-4 justify-center items-center"}>
           <div className={"mb-20"}>
             <Image width={200} height={200} alt={"logo"} src={"/logo.svg"} className={"bg-black"}/>
@@ -56,32 +54,32 @@ const Page = async () => {
         </div>
       </div>
       <div className={"h-[135px] px-4 flex justify-start items-center flex-col text-[13px] text-[#737373]"}>
-        <div className={"flex gap-4 mt-4"}>
-          <Link href={"/about.md"} target={"_blank"} className={"hover:underline"}>
+        <div className={"flex gap-4 mt-4 w-screen overflow-scroll px-4 md:w-fit"}>
+          <Link href={"/about.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             About
           </Link>
-          <Link href={"https://x.com/abandonai"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"https://x.com/abandonai"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             X.com
           </Link>
-          <Link href={"/careers.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/careers.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             Careers
           </Link>
-          <Link href={"/help.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/help.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             Help
           </Link>
-          <Link href={"/api.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/api.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             API
           </Link>
-          <Link href={"/mcp.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/mcp.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             MCP
           </Link>
-          <Link href={"/privacy-policy.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/privacy-policy.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             Privacy Policy
           </Link>
-          <Link href={"/safety.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/safety.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             Safety
           </Link>
-          <Link href={"/terms-of-use.md"} target={"_blank"} className={"hover:underline"}>
+          <Link href={"/terms-of-use.md"} target={"_blank"} className={"hover:underline whitespace-nowrap"}>
             Terms of Use
           </Link>
         </div>
