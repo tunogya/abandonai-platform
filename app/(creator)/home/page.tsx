8813339f342335f1ed1c0e28274628e86fc4695a
tunogya <tunogya@qq.com>
2025-04-07 16:27:25 +0800

    update

--- app/(creator)/home/page.tsx
+++ app/(creator)/home/page.tsx
@@ -37,7 +37,7 @@ const Page = () => {
       <div
         className={"w-full flex flex-col md:pt-0 md:border-transparent dark:border-white/20 md:dark:border-transparent"}>
         <div>
-          <div className={"text-base mx-auto px-3 md:px-4 w-full lg:px-4 xl:px-5"}>
+          <div className={"text-base mx-auto px-3 pt-3 md:px-4 w-full lg:px-4 xl:px-5"}>
             <div
               className={"mx-auto flex flex-1 text-base gap-4 md:gap-5 lg:gap-6 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"}>
               <form onSubmit={handleSubmit} className={"w-full border border-[#DBDBDB] rounded-full px-4 py-2"}>
@@ -47,7 +47,7 @@ const Page = () => {
                   placeholder="Send a message..."
                   onChange={handleInputChange}
                   disabled={status !== 'ready'}
-                  className={"w-full active:border-none active:outline-none focus:outline-none focus:border-none text-base placeholder-[#65676B]"}
+                  className={"w-full active:border-none active:outline-none focus:outline-none focus:border-none text-base placeholder-[#65676B] bg-transparent"}
                 />
               </form>
             </div>
