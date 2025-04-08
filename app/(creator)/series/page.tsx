6ede15cea5e8966f7b0f73a20f2b851a601e6022
tunogya <tunogya@qq.com>
2025-04-08 11:46:01 +0800

    update

--- app/(creator)/series/page.tsx
+++ app/(creator)/series/page.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
+import {getTranslations} from "next-intl/server";
 
-const Page = () => {
+const Page = async () => {
+  const t = await getTranslations("Series");
+
   return (
     <div className={"md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] mx-auto"}>
       <div className={"py-4"}>
-        <div className={"text-3xl font-bold py-4"}>Series</div>
+        <div className={"text-3xl font-bold py-4"}>{t("Series")}</div>
       </div>
       <div className={"flex items-center justify-between border border-[#DBDBDB] p-8 rounded-2xl"}>
         <div className={""}>
-          You haven’t created any series yet.
+          {t("You haven’t created any series yet")}
         </div>
         <Link href={"/create"} prefetch className={"hover:bg-foreground hover:text-background px-8 h-12 font-bold rounded-full border border-[#DBDBDB] flex items-center justify-center"}>
-          Create
+          {t("Create")}
         </Link>
       </div>
     </div>
