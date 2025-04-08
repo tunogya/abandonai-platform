d946e33bd806c8134234601eb2b0b2bcf52ea17e
tunogya <tunogya@qq.com>
2025-04-08 12:11:05 +0800

    update

--- app/_lib/twitter.ts
+++
@@ -1,8 +0,0 @@
-import {TwitterApi} from 'twitter-api-v2';
-
-const twitterClient = new TwitterApi({
-  clientId: process.env.X_CLIENT_ID || "",
-  clientSecret: process.env.X_CLIENT_SECRET || "",
-});
-
-export {twitterClient}
