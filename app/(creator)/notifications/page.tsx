30adcdf4c25cb0db5142909356f0ce991509fe8f
tunogya <tunogya@qq.com>
2025-04-08 11:48:52 +0800

    update

--- app/(creator)/notifications/page.tsx
+++ app/(creator)/notifications/page.tsx
@@ -1,8 +1,12 @@
-const Page = () => {
+import {getTranslations} from "next-intl/server";
+
+const Page = async () => {
+  const t = await getTranslations("Notifications");
+
   return (
     <div className={"mx-auto p-8 relative min-h-screen w-full"}>
       <div className={"text-3xl font-bold"}>
-        通知
+        {t("Notifications")}
       </div>
     </div>
   )
