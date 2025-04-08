c22a5c1bbe4d718368674617c7f48da2b7c0b8f1
tunogya <tunogya@qq.com>
2025-04-08 11:20:38 +0800

    update

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,30 @@
 import {getRequestConfig} from 'next-intl/server';
+import { cookies, headers } from 'next/headers';
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = 'en';
+  const supportedLocales = ['en'];
+
+  let locale = 'en'; // default fallback
+  // Try to get locale from cookies
+  const localeFromCookie = (await cookies()).get('locale')?.value;
+  if (localeFromCookie) {
+    // If in cookies, check if it's supported
+    if (supportedLocales.includes(localeFromCookie)) {
+      locale = localeFromCookie;
+    }
+  } else {
+    // If not in cookies, try headers
+    const acceptLanguage = (await headers()).get('accept-language');
+    if (acceptLanguage) {
+      for (const lang of acceptLanguage.split(', ')) {
+        const langCode = lang.split('-')[0];
+        if (supportedLocales.includes(langCode)) {
+          locale = langCode;
+          break;
+        }
+      }
+    }
+  }
 
   return {
     locale,
